0e0344a17aa278ce1ea6bdfba2fd97e527fcfdbc
Ali Hejazizo <hejazizo@ualberta.ca>
2021-12-12 18:10:15 -0700

    add filters

--- src/bot.py
+++ src/bot.py
@@ -2,7 +2,7 @@ import os
 import telebot
 
 
-# initialize bot
+# Initialize bot
 bot = telebot.TeleBot(
-	os.environ['NASHENAS_BOT_TOKEN'], parse_mode='HTML'
+    os.environ['NASHENAS_BOT_TOKEN'], parse_mode='HTML'
 )
--- src/constants.py
+++ src/constants.py
@@ -1,12 +1,13 @@
 from types import SimpleNamespace
 
 from src.utils.keyboard import create_keyboard
+import emoji
 
 keys = SimpleNamespace(
-    random_connect=':bust_in_silhouette: Random Connect',
-    settings=':gear: Settings',
+    settings=emoji.emojize(':gear: Settings'),
+    exit=emoji.emojize(':cross_mark: Exit'),
 )
 
 keyboards = SimpleNamespace(
-    main=create_keyboard(keys.random_connect, keys.settings),
+    main=create_keyboard(keys.settings),
 )
